a8cef15804e0e53ede29ab953823965c28c3285b
DESKTOP-2L5S43O\Wandr <wandry.oscar@gmail.com>
2023-05-07 10:11:45 -0400

    Add: modules changes to component

--- app/components/footer/footer.js
+++ app/components/footer/footer.js
@@ -1,19 +1,24 @@
 import styles from '../../assets/scss/components/common/_footer.module.scss';
+import Script from 'next/script';
 
 export default function Footer() {
     return (
-        <footer className={styles.footer}>
-            <div className="footer__container container">
-                <section className={styles.section}>
-                    <div className={styles.description}>
-                        <h3 className={styles.title}>By Wandry Martinez</h3>
-                        <p><i>Nexjs</i></p>
-                    </div>
-                    <div className="footer__social">
-                        redes sociales
-                    </div>
-                </section>
-            </div>
-        </footer>
+        <>
+            <footer className={styles.footer}>
+                <div className="footer__container container">
+                    <section className={styles.section}>
+                        <div className={styles.description}>
+                            <h3 className={styles.title}>By Wandry Martinez</h3>
+                            <p><i>Nexjs</i></p>
+                        </div>
+                        <div className="footer__social">
+                            redes sociales
+                        </div>
+                    </section>
+                </div>
+            </footer>
+            <Script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" />
+        </>
+
     )
 }
